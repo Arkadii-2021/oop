44b184981756145c3a652a7ee07c4a402d4a51e0
Arkadii Kozhuhow <arkadii_kozhuhow@mail.ru>
2022-06-24 20:19:49 +0700

    oop -hw

---
+++ good.js
@@ -0,0 +1,140 @@
+class Good {
+	id;
+	name;
+	description;
+	sizes;
+	price;
+	available;
+	
+	constructor (id, name, description, sizes, price, available) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.sizes = sizes;
+		this.price = price;
+		this.available = available;
+	};
+};
+
+
+class GoodsList {
+	constructor (goods, goodsFiltered, filter, sortPrice, sortDir) {
+		this.goods = [];
+		this.goodsFiltered = [];
+		this.filter = filter;
+		this.sortPrice = sortPrice;
+		this.sortDir = sortDir;	
+	};
+	
+	sortedPrice (sortPrice, sortDir) {
+		this.sortPrice = sortPrice;
+		this.sortDir = sortDir;
+		let logbar = ['Сортировка по возрастанию', 'Сортировка по убыванию', 'Сортировка выключена'];
+		if (sortPrice == true && sortDir == true) {
+			document.write(logbar[0]);
+			this.goodsFiltered.sort(function(a, b) {
+			return a.price - b.price;
+			});
+			
+		} else if (sortPrice == true && sortDir == false) {
+			document.write(logbar[1]);
+			this.goodsFiltered.sort(function(a, b) {
+			return b.price - a.price;
+			});
+			
+		} else document.write(logbar[2]);
+	
+};
+	
+	filterName (filter) {
+		this.filter = filter;
+		this.goods.forEach(function (good, indexIdReal) {
+			let str = f1.goods[indexIdReal].name;
+			let re = new RegExp(filter, 'i');
+			let found = str.match(re);
+			if (found) {
+				f1.goodsFiltered.push(f1.goods[indexIdReal]);
+	}})};	
+
+	add (id, name, description, sizes, price, available) {
+		this.goods.push({id: id, name: name, description: description, sizes: sizes, price: price, available: available});
+	};
+	
+	remove (productId) {
+		this.goods.forEach(function (good, indexIdReal) {
+			if (good.id === productId) {
+			f1.goods.splice(indexIdReal, 1);
+			}
+		})
+	};
+
+	get list () {
+		return this.goodsFiltered;
+	}
+	
+};
+
+
+class Basket extends GoodsList {
+	constructor (id, name, description, sizes, price, available, amount, basket, goods) {
+		super(id, name, description, sizes, price, available, goods);
+		this.basket = [];
+	};
+	
+	add (ids, amount) {
+		f1.goods.forEach(function (goodsID, indexIdReal) {
+			if (goodsID.id === ids){
+			b1.basket.push(f1.goods[ids - 1]);
+			goodsID.amount = amount;
+			}
+		});	
+	};
+	
+	remove (basketIDS) {
+		this.basket.forEach(function (basketID, indexIdReal) {
+			if (basketID.id === basketIDS) {
+			b1.basket.splice(indexIdReal, 1);
+			}
+		});	
+	};
+
+	clear() {
+		this.basket.length = 0;
+	}
+	
+}
+
+/* b1 - экземпляр класса GoodsList*/
+const f1 = new GoodsList();
+
+/* Добавление товаров в массив goods */
+f1.add(1, 'ФУТБОЛКА ANTISOCIAL BASIC ФИОЛЕТОВЫЙ', 'Футболка выполнена из 100% хлопка.', [], 1650, 1);
+f1.add(2, 'ШТАНЫ СПОРТИВНЫЕ ПРЯМЫЕ ANTISOCIAL STRIPE ЧЕРНЫЙ', '– Прямой крой, стандартная посадка, передние карманы на молнии, эластичный пояс со шнурком, лампасы', [], 3520, 1);
+f1.add(3, 'РУБАШКА URBAN CLASSICS CHECKED ROOTS SHIRT DARKBLUE/WHITE', 'Клетчатая рубашка свободного покроя из тонкой фланели.', [], 6990, 1);
+f1.add(4, 'ОЛИМПИЙКА ANTISOCIAL STRIPE СЕРЫЙ', 'Слегка зауженный крой, минималистичный логотип на груди, лампасы на рукавах, два боковых кармана на замках', [], 3740, 1);
+f1.add(5, 'РЮКЗАК ЯКОРЬ МПА ПРОДВИНУТАЯ БАЗА ВС ТЕМНО-СИНИЙ НЕЙЛОН-1000', 'Описание отсутствует', [], 5050, 1);
+
+/* фильтрация, сортировка, удаление товаров и вывод в консоль массива*/
+f1.filterName('STRIPE');
+f1.sortedPrice(true, false);
+/* f1.remove(3); */
+
+
+/* b1 - экземпляр класса корзины*/
+const b1 = new Basket();
+
+/* добавление/удаление товаров в корзину/из корзины с указанием количества*/
+b1.add(2, 1);
+b1.add(5, 3);
+b1.add(4, 2);
+b1.add(2, 5);
+/* b1.remove(2); */
+
+/* очистка корзины*/
+/* b1.clear(); */
+
+
+/* мониторинг на корректность */
+console.log(f1);
+console.log(b1);
+console.log(f1.list);
